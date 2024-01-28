6c48dc4255699ae73e3eb40b941d8899981f437f
Mohammod Ibrahim <143773181+MohammoD2@users.noreply.github.com>
2024-01-28 13:43:50 +0600

    Add files via upload

---
+++ app_config.py
@@ -0,0 +1,11 @@
+app_config = {
+    "title": "Welcome To Ibrahim Creation",
+    "creator": "Mohammod Ibrahim Hossain",
+    "model_path": "/mount/src/movie/Movie_dict1.pkl",
+    "similarity_path": "/mount/src/Movie/similarity1.pkl",
+    "poster_api_key": "8265bd1679663a7ea12ac168da84d2e8",
+    "poster_base_url": "https://api.themoviedb.org/3/movie/{}?api_key={}&language=en-US",
+    "poster_image_url": "https://image.tmdb.org/t/p/w500/{}",
+    "num_recommendations": 5,
+}
+
